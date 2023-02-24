0e8c4601c37e179f663ffbfe13a540b9042a41e0
MEDHASRI KONDAMURI <88303507+MedhaMouli@users.noreply.github.com>
2023-02-24 17:18:35 +0530

    Add files via upload

---
+++ ex01_Chrome.py
@@ -0,0 +1,19 @@
+import unittest
+from selenium import webdriver
+import time
+
+class MyTestCase(unittest.TestCase):
+    def test_launch_browser(self):
+        filePath = "C:\\Users\\KMEDHASR\\PycharmProjects\\Selenium\\Drivers\\chromedriver.exe"
+
+        #global driver
+        # If you will make driver global then you need to close/quit the browser explicitly
+
+        driver = webdriver.Chrome(filePath)
+        time.sleep(2)
+
+        # No need to close/quit the browser as browser will close automatically
+        #driver.quit()
+
+    if __name__ == '__main__':
+        unittest.main()
